4a869c9449368740c8c6fe7664bc8c972460c42d
zhouxiaojie96 <3290459311@qq.com>
2019-12-13 10:22:13 +0800

    项目优化了跳转时的进度条，解决了生产环境下的错误

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-13 09:42:11
+ * @LastEditTime: 2019-12-13 10:16:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_shop\babel.config.js
+ */
+
+//用于项目发布阶段用到的 babel 插件
+const prodPlugins = []
+//判断当前项目阶段是否是发布阶段
+if(process.env.NODE_ENV === "production"){
+//transform-remove-console 功能是把项目中的console代码都去掉
+  prodPlugins.push("transform-remove-console")
+}
+
 module.exports = {
   'presets': [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +26,7 @@ module.exports = {
         'libraryName': 'element-ui',
         'styleLibraryName': 'theme-chalk'
       }
-    ]
+    ],
+    ...prodPlugins//... 是展开运算符，是把 数组中的每一项都展开了放到另一个数组中
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-11-20 11:22:45
- * @LastEditTime: 2019-12-11 15:08:34
+ * @LastEditTime: 2019-12-13 10:03:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_shop\src\main.js
@@ -28,13 +28,29 @@ import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 Vue.use(VueQuillEditor)//将富文本编辑器注册为全局可用的组件
 
+//nprogress可以显示隐藏发起请求的进度条
+//在请求拦截器中展示进度条，在响应拦截器中隐藏进度条
+//1.导入nprogress包和样式
+import NProgress from 'nprogress'
+//这里把样式表注释了，因为后期项目优化需要，然后这个样式表修改到index.html文件里引入了cdn
+import 'nprogress/nprogress.css'
+
 //使用axios配置方法
 import axios from 'axios'
 import { Tree } from 'element-ui'
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/" //全局配置发起请求的根路径
 //请求拦截器
 axios.interceptors.request.use(config => {
-  console.log(config);
+  //当发起请求时就展示进度条
+  NProgress.start()//展示进度条
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+})
+
+//响应拦截器
+axios.interceptors.response.use(config => {
+  //当有响应了之后就隐藏进度条
+  NProgress.done()//隐藏进度条
   config.headers.Authorization = window.sessionStorage.getItem("token");
   return config;
 })
