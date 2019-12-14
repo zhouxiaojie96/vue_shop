f6e312f937cabdc188b1b8759d3cb69278dd84de
zhouxiaojie96 <3290459311@qq.com>
2019-12-14 10:02:09 +0800

    优化了打包项目的大小，改成了引入cdn的方式

---
+++ src/main-prod.js
@@ -0,0 +1,67 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-20 11:22:45
+ * @LastEditTime: 2019-12-14 09:59:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_shop\src\main.js
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// import './plugins/element.js' //导入elementsUi组件//导入elementsUi组件，这里注释掉了是因为后期项目优化需要，然后这个文件挪动到index.html文件里引入了CDN的形式
+import "./assets/css/global.css" //导入全局样式表
+import "./assets/fonts/iconfont.css" //导入全局样式
+import './filter/filter'//导入过滤器
+
+//treetable使用方法
+import TreeTable from 'vue-table-with-tree-grid'//树样式的插件
+Vue.component("tree-table",TreeTable) //注册组件
+//Vue.use(TreeTable)//这个方法不能起名字，也没有那么正规，最正规的还是vue.component方式。
+
+//vue-quill-editor富文本使用方法
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本全局样式
+//这里把样式表注释了，因为后期项目优化需要，然后这个样式表修改到index.html文件里引入了cdn
+// import 'quill/dist/quill.core.css'
+// import 'quill/dist/quill.snow.css'
+// import 'quill/dist/quill.bubble.css'
+Vue.use(VueQuillEditor)//将富文本编辑器注册为全局可用的组件
+
+//nprogress可以显示隐藏发起请求的进度条
+//在请求拦截器中展示进度条，在响应拦截器中隐藏进度条
+//1.导入nprogress包和样式
+import NProgress from 'nprogress'
+//这里把样式表注释了，因为后期项目优化需要，然后这个样式表修改到index.html文件里引入了cdn
+// import 'nprogress/nprogress.css'
+
+//使用axios配置方法
+import axios from 'axios'
+import { Tree } from 'element-ui'
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/" //全局配置发起请求的根路径
+//请求拦截器
+axios.interceptors.request.use(config => {
+  //当发起请求时就展示进度条
+  NProgress.start()//展示进度条
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+})
+
+//响应拦截器
+axios.interceptors.response.use(config => {
+  //当有响应了之后就隐藏进度条
+  NProgress.done()//隐藏进度条
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+})
+//给Vue函数添加一个原型属性$axios 指向axios；这样做的好处是在vue实例或组件中不用再去重复引用Axios 直接用this.$http就能执行axios 方法了。
+Vue.prototype.$http = axios;
+
+//自带的
+Vue.config.productionTip = false
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,43 @@
 /*
  * @Author: your name
  * @Date: 2019-11-20 19:36:15
- * @LastEditTime: 2019-11-20 19:36:50
- * @LastEditors: your name
+ * @LastEditTime: 2019-12-13 10:28:32
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_shop\vue.config.js
  */
 module.exports = {
-    devServer:{
-        //自动打开浏览器
-        open : true
-    }
+  devServer: {
+    //自动打开浏览器
+    open: true
+  }
+}
+
+//项目优化分环境打包
+
+module.exports = {
+  //生成一个配置节点，指向的值是function处理函数，通过config接受默认的webpack配置对象
+  chainWebpack: config => {
+    //通过when 判断当前是什么阶段的编译模式。
+    //通过 entry 得到默认的打包入口，然后再通过 clear() 清空掉，然后再通过add新增一个自己的打包入口
+    //产品发布阶段如何自定义打包入口
+    config.when(process.env.NODE_ENV === 'production', config => {
+      //这里面都是项目的依赖包
+      config.set('externals', {
+        vue: 'vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        lodash: '_',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        'vue-quill-editor': 'vueQuillEditor'
+      })
+
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+    //产品开发阶段如何自定义打包入口
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 }
